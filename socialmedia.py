ee3f20fd53c6fc73d9622044418f62f29c729677
Thizz00 <97180137+Thizz00@users.noreply.github.com>
2022-06-28 16:01:34 +0200

    Delete socialmedia.py

--- socialmedia.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-class smedia(object):
-    def fb():
-        return(webbrowser.open("https://www.facebook.com/profile.php?id=100004259290790&sk=about"))
-    def yt():
-            return(webbrowser.open("https://www.youtube.com/channel/UCzLnIfC562M6ZgI2V3Y3wnQ"))
-    def browse():
-            webbrowser.open("https://jakubkielb.netlify.app")
